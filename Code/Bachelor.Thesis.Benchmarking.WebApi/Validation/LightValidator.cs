53a18ee44ea5c1c199d86f7a2b68953546a564e7
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-05 15:29:20 +0200

    Added base lightValidation class

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Validation/LightValidator.cs
@@ -0,0 +1,18 @@
+﻿using Light.Validation;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Validation;
+
+public class LightValidator<T>
+{
+    private readonly T _itemToValidate;
+    private readonly Validator<T> _validator;
+
+    public LightValidator(Validator<T> validator, T itemToValidate)
+    {
+        _validator = validator;
+        _itemToValidate = itemToValidate;
+    }
+
+    public ValidationResult<T> PerformValidation() =>
+        _validator.Validate(_itemToValidate);
+}
