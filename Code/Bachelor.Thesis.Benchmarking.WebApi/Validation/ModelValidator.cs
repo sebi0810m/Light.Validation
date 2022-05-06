cda4558023c3d1bd4d0169afeaf3d9a76ba6fde8
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-06 12:13:09 +0200

    Added base modelValidation class

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Validation/ModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Validation;
+
+public class ModelValidator<T>
+{
+    private readonly T _itemToValidate;
+
+    public ModelValidator(T itemToValidate)
+    {
+        _itemToValidate = itemToValidate;
+    }
+
+    public List<ValidationResult> PerformValidation()
+    {
+        var errors = new List<ValidationResult>();
+        Validator.TryValidateObject(_itemToValidate!, new ValidationContext(_itemToValidate!), errors, true);
+        return errors;
+    }
+}
