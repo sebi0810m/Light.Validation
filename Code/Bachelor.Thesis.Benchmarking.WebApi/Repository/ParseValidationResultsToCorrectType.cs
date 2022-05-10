2bb25c8a7414ed6867b2b9b71e989664f077dc55
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-10 11:05:16 +0200

    Implemented basic class for parsing validation results to correct type for http response

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Repository/ParseValidationResultsToCorrectType.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+using Light.Validation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Repository;
+
+public static class ParseValidationResultsToCorrectType
+{
+    public static ProblemDetails ParseLightValidationResults<T>(ValidationResult<T> validationResult)
+    {
+        return new ProblemDetails();
+    }
+
+    public static ProblemDetails ParseFluentValidationResults(ValidationResult validationResult)
+    {
+        return new ProblemDetails();
+    }
+
+    public static ProblemDetails ParseModelValidationResults(List<ValidationResult> validationResult)
+    {
+        return new ProblemDetails();
+    }
+}
