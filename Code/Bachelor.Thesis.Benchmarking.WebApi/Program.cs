fa6c6dc7150083ebaf735f862fee808496ee07e6
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-10 10:31:18 +0200

    Setup for first api endpoint, included dependency injection for repository

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Program.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
+using Bachelor.Thesis.Benchmarking.WebApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Bachelor.Thesis.Benchmarking.WebApi;
 
 public class Program
@@ -5,14 +9,26 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddSingleton<ParametersPrimitiveTwoRepo>();
 
         var app = builder.Build();
 
         app.UseHttpsRedirection();
 
-        app.UseRouting();
-
         app.MapGet("", () => "Hello World");
+
+        app.MapPost("/api/primitive/two/light", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) =>
+        {
+            try
+            {
+                var createdUser = repo.CreateWithLightValidation(user);
+                return Results.Created($"/api/primitive/two/{user.Id}", user);
+            }
+            catch (Exception ex)
+            {
+                return Results.ValidationProblem()
+            }
+        });
 
         app.Run();
     }
