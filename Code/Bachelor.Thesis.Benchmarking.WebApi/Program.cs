17f671afde9605b7ab338c21eb0e1d146fea236e
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-10 14:27:53 +0200

    Fixed false method call in api routings

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Program.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Program.cs
@@ -17,9 +17,9 @@ public class Program
 
         app.MapGet("", () => "Hello World");
 
-        app.MapPost($"{ParametersPrimitiveTwoRepo.Url}light", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithFluentValidation(user));
+        app.MapPost($"{ParametersPrimitiveTwoRepo.Url}light", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithLightValidation(user));
         app.MapPost($"{ParametersPrimitiveTwoRepo.Url}fluent", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithFluentValidation(user));
-        app.MapPost($"{ParametersPrimitiveTwoRepo.Url}model", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithFluentValidation(user));
+        app.MapPost($"{ParametersPrimitiveTwoRepo.Url}model", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithModelValidation(user));
 
         app.Run();
     }
