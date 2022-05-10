0acb9ca4ccd990763decd019f4217e36bc496e78
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-10 10:29:46 +0200

    Implemented basic repository interface for essential api methods

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Bachelor.Thesis.Benchmarking.WebApi.Repository;
+
+public interface IRepository<T>
+{
+    public T CreateWithFluentValidation(T value);
+
+    public T CreateWithLightValidation(T value);
+
+    public T CreateWithModelValidation(T value);
+}
