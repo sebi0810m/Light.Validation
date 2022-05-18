dab25d8e9144d55e8ed27d8ce865a445efb881ff
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-18 10:58:54 +0200

    Implemented first macro test for primitive dto and light validation

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Tests/ParametersPrimitiveTwo/LightValidTest.js
@@ -0,0 +1,19 @@
+﻿import http from 'k6/http';
+
+export let options = {
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    vus: 1,
+    duration: '10s'
+}
+
+export default function() {
+    const url = 'https://localhost:7089/api/primitive/two/light';
+
+    const payload = JSON.stringify({
+        id: '42',
+        name: 'John Doe'
+    });
+
+    http.post(url, payload);
+}
