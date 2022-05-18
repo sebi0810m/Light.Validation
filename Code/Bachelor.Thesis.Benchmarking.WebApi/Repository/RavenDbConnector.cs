3883d76d9e28757b874d4272432a2cd27e07132c
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-18 14:05:40 +0200

    Added RavenDb as database for WebApi benchmarks

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Repository/RavenDbConnector.cs
@@ -0,0 +1,32 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
+using Raven.Client.Documents;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Repository;
+
+public static class RavenDbConnector
+{
+    public static IDocumentStore SetupRavenDbConnection()
+    {
+        using var store = new DocumentStore
+        {
+            Urls = new[]
+            {
+                "http://127.0.0.1:1337/"
+            },
+            Database = "Northwind"
+        };
+
+        store.Initialize();
+
+        return store;
+    }
+
+    public static UserDto StoreNewUserDtoInDatabase(this UserDto user, IDocumentStore store)
+    {
+        using var session = store.OpenSession();
+
+        session.Store(user);
+
+        return user;
+    }
+}
