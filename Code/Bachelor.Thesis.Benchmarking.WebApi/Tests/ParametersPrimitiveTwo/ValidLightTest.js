a160ad6bfd1125a794037aac4d1a8ffe427f9516
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-19 15:18:08 +0200

    Cleaned up JS file

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Tests/ParametersPrimitiveTwo/ValidLightTest.js
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Tests/ParametersPrimitiveTwo/ValidLightTest.js
@@ -1,18 +1,18 @@
-﻿import http from 'k6/http';
+﻿import http from "k6/http";
 
 export let options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
     vus: 1,
-    duration: '10s'
+    duration: "10s"
 }
 
 export default function() {
-    const url = 'https://localhost:7089/api/primitive/two/light';
+    const url = "https://localhost:7089/api/primitive/two/light";
 
     const payload = JSON.stringify({
-        id: '42',
-        name: 'John Doe'
+        id: "42",
+        name: "John Doe"
     });
 
     http.post(url, payload);
