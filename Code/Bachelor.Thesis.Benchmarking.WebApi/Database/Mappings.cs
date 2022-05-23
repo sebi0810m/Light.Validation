2a71abfd9c06f7fb550a9ccf973a80f522fbf7e7
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-23 13:57:51 +0200

    Added mappings for database tables

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Database/Mappings.cs
@@ -0,0 +1,31 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
+using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
+using LinqToDB.Mapping;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Database;
+
+public static class Mappings
+{
+    public static void CreateMappings(this MappingSchema schema)
+    {
+        var mappingBuilder = schema.GetFluentMappingBuilder();
+
+        mappingBuilder.Entity<UserDto>()
+                      .HasTableName("ParametersPrimitiveTwo")
+                      .Property(user => user.Id).IsIdentity().IsPrimaryKey()
+                      .Property(user => user.Name).IsNullable(false);
+
+        mappingBuilder.Entity<EmployeeDto>()
+                      .HasTableName("ParametersPrimitiveAll")
+                      .Property(employee => employee.Id).IsIdentity().IsPrimaryKey()
+                      .Property(employee => employee.Name).IsNullable(false)
+                      .Property(employee => employee.Position).IsNullable(false)
+                      .Property(employee => employee.Department).IsNullable(false)
+                      .Property(employee => employee.WeeklyWorkingHours).IsNullable(false)
+                      .Property(employee => employee.EmployeeId).IsNullable(false)
+                      .Property(employee => employee.ProductivityScore).IsNullable(false)
+                      .Property(employee => employee.OvertimeWorked).IsNullable(false)
+                      .Property(employee => employee.HourlySalary).IsNullable(false)
+                      .Property(employee => employee.DateEmployed).IsNullable(false);
+    }
+}
