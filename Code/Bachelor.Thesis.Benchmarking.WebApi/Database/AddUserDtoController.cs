443f8ffcad5f4b65e06c6b3d49781025d79d3d6b
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-25 10:52:56 +0200

    Added controller for dependency injection and mapping factory methods

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Database/AddUserDtoController.cs
@@ -0,0 +1,23 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
+using Bachelor.Thesis.Benchmarking.WebApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Database;
+
+public static class AddUserDtoController
+{
+    public static IServiceCollection AddInsertUserDto(this IServiceCollection services) =>
+        services.AddSessionFactoryFor<IAddUserSession, LinqToDbAddUserSession>();
+
+    public static WebApplication MapInsertUserDto(this WebApplication app)
+    {
+        var defaultUrl = ParametersPrimitiveTwoRepo.Url;
+
+        app.MapPost($"{defaultUrl}light", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithLightValidation(user));
+        app.MapPost($"{defaultUrl}fluent", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithFluentValidation(user));
+        app.MapPost($"{defaultUrl}model", ([FromServices] ParametersPrimitiveTwoRepo repo, UserDto user) => repo.CreateWithModelValidation(user));
+
+        return app;
+    }
+}
