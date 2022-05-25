51ea21ffcb0d9da349fc271c53e20b3c2cdca83f
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-25 10:52:28 +0200

    Added classes for inserting new UserDtos in database

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Database/IAddUserSession.cs
@@ -0,0 +1,9 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
+using Synnotech.DatabaseAbstractions;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Database;
+
+public interface IAddUserSession : IAsyncReadOnlySession
+{
+    public Task<int> InsertUserAsync(UserDto user);
+}
---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Database/LinqToDbAddUserSession.cs
@@ -0,0 +1,16 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
+using LinqToDB;
+using LinqToDB.Data;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Database;
+
+public class LinqToDbAddUserSession : AsyncReadOnlySession, IAddUserSession
+{
+    public LinqToDbAddUserSession(DataConnection dataConnection) : base(dataConnection) { }
+
+    public Task<int> InsertUserAsync(UserDto user)
+    {
+        return DataConnection.InsertWithInt32IdentityAsync(user);
+    }
+}
