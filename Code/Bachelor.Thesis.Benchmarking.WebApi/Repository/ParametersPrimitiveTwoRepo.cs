c42330d925037a463780b812aa4b867dec2f1d6d
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-05-30 17:09:08 +0200

    Made http endpoint method async

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Repository/ParametersPrimitiveTwoRepo.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Repository/ParametersPrimitiveTwoRepo.cs
@@ -1,22 +1,16 @@
 ﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
 using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo.Validators;
+using Bachelor.Thesis.Benchmarking.WebApi.Database;
 using Bachelor.Thesis.Benchmarking.WebApi.Validation;
+using Microsoft.AspNetCore.Mvc;
 using Synnotech.AspNetCore.MinimalApis.Responses;
+using Synnotech.DatabaseAbstractions;
 
 namespace Bachelor.Thesis.Benchmarking.WebApi.Repository;
 
 public class ParametersPrimitiveTwoRepo : IRepository<UserDto>
 {
     public static string Url = "/api/primitive/two/";
-
-    public IResult CreateWithFluentValidation(UserDto value)
-    {
-        var errors = new FluentValidator<UserDto>(new FluentValidator(), value).PerformValidation();
-        if (!errors.IsValid)
-            return Response.ValidationProblem(ParseValidationResultsToCorrectType.ParseFluentValidationResults(errors));
-
-        return Response.Created($"{Url}{value.Id}", value);
-    }
 
     public IResult CreateWithLightValidation(UserDto value)
     {
@@ -34,6 +28,19 @@ public class ParametersPrimitiveTwoRepo : IRepository<UserDto>
 
         if (errors.Count != 0)
             return Response.ValidationProblem(ParseValidationResultsToCorrectType.ParseModelValidationResults(errors));
+
+        return Response.Created($"{Url}{value.Id}", value);
+    }
+
+    public async Task<IResult> CreateWithFluentValidation(
+        [FromServices] ISessionFactory<IAddUserSession> sessionFactory,
+        UserDto value)
+    {
+        var errors = new FluentValidator<UserDto>(new FluentValidator(), value).PerformValidation();
+        if (!errors.IsValid)
+            return Response.ValidationProblem(ParseValidationResultsToCorrectType.ParseFluentValidationResults(errors));
+
+        await using var session = await sessionFactory.OpenSessionAsync();
 
         return Response.Created($"{Url}{value.Id}", value);
     }
