52acdfc235efb4e4f1902bb72fe68fe666103219
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-01 11:16:02 +0200

    Added interface for getUser session

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveTwo/IGetUserSession.cs
@@ -0,0 +1,9 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
+using Synnotech.DatabaseAbstractions;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveTwo;
+
+public interface IGetUserSession : IAsyncReadOnlySession
+{
+    Task<UserDto?> GetUserByIdAsync(int userId);
+}
