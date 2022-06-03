fe044e1c5fcb270958bb91025ad63f2a6d99ae98
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-03 15:50:01 +0200

    Added interfaces for AllPrimitive-Case sessions

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/IAddEmployeeSession.cs
@@ -0,0 +1,9 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
+using Synnotech.DatabaseAbstractions;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
+
+public interface IAddEmployeeSession : IAsyncSession
+{
+    public Task<int> InsertEmployeeAsync(EmployeeDto employee);
+}
