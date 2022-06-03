c8f0f8eaf256b833ce6e8a150ea0eeeb9c22d4a5
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-03 15:20:55 +0200

    Added mappings for all main Dtos

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Database/Mappings.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Database/Mappings.cs
@@ -1,4 +1,7 @@
-﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
+﻿using Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
+using Bachelor.Thesis.Benchmarking.CollectionFlat;
+using Bachelor.Thesis.Benchmarking.ParametersComplexTwo.Dto;
+using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
 using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
 using LinqToDB.Mapping;
 
@@ -10,11 +13,13 @@ public static class Mappings
     {
         var mappingBuilder = schema.GetFluentMappingBuilder();
 
+        // ParametersPrimitiveTwo
         mappingBuilder.Entity<UserDto>()
                       .HasTableName("ParametersPrimitiveTwo")
                       .Property(user => user.Id).IsIdentity().IsPrimaryKey()
                       .Property(user => user.Name).IsNullable(false);
 
+        // ParametersPrimitiveAll
         mappingBuilder.Entity<EmployeeDto>()
                       .HasTableName("ParametersPrimitiveAll")
                       .Property(employee => employee.Id).IsIdentity().IsPrimaryKey()
@@ -27,5 +32,24 @@ public static class Mappings
                       .Property(employee => employee.OvertimeWorked).IsNullable(false)
                       .Property(employee => employee.HourlySalary).IsNullable(false)
                       .Property(employee => employee.DateEmployed).IsNullable(false);
+
+        // ParametersComplexTwo
+        mappingBuilder.Entity<CustomerDto>()
+                      .HasTableName("ParametersComplexTwo")
+                      .Property(customer => customer.Address).IsNullable(false)
+                      .Property(customer => customer.User).IsNullable(false)
+                      .Property(customer => customer.User.UserName).IsPrimaryKey();
+        
+        // CollectionFlat
+        mappingBuilder.Entity<CollectionFlatDto>()
+                      .HasTableName("CollectionFlat")
+                      .Property(collection => collection.Names).IsNullable(false)
+                      .Property(collection => collection.Availability).IsNullable(false);
+
+        // CollectionComplex
+        mappingBuilder.Entity<CollectionComplexDto>()
+                      .HasTableName("CollectionComplex")
+                      .Property(collection => collection.OrderDetailsList).IsNullable(false)
+                      .Property(collection => collection.ArticleList).IsNullable(false);
     }
 }
