71105807be2fabb13a989ed86a56d1a46406fab8
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-03 15:50:26 +0200

    Added basic Repo for AllPrimitive-case

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/ParametersPrimitiveAllRepo.cs
@@ -0,0 +1,29 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
+using Bachelor.Thesis.Benchmarking.WebApi.Repository;
+using Synnotech.DatabaseAbstractions;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
+
+public class ParametersPrimitiveAllRepo : IRepository<EmployeeDto, IAddEmployeeSession, IGetEmployeeSession>
+{
+    public const string Url = "/api/primitive/all/";
+    public Task<IResult> CreateWithFluentValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> CreateWithLightValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> CreateWithModelValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> GetObjectByIdAsync(int id, ISessionFactory<IGetEmployeeSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+}
