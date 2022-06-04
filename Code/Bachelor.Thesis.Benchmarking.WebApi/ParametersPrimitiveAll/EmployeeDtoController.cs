8b9aaa7af6d0824f05fe3dde50ca6fa3135d26c9
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-04 15:11:21 +0200

    Implemented controller for employeeDto

--- Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/EmployeeDtoController.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/EmployeeDtoController.cs
@@ -1,10 +1,24 @@
-﻿using Synnotech.Linq2Db;
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
+using Microsoft.AspNetCore.Mvc;
+using Synnotech.DatabaseAbstractions;
+using Synnotech.Linq2Db;
 
 namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
 
 public static class EmployeeDtoController
 {
-    public static IServiceCollection AddEmployeeDtoServices(this IServiceCollection services) => services;
+    public static IServiceCollection AddEmployeeDtoServices(this IServiceCollection services) =>
+        services.AddSessionFactoryFor<IAddEmployeeSession, LinqToDbAddEmployeeSession>();
 
+    public static WebApplication AddEmployeeDtoEndpoints(this WebApplication app)
+    {
+        var defaultUrl = ParametersPrimitiveAllRepo.Url;
 
+        app.MapPost($"{defaultUrl}fluent", async (
+                        [FromServices] ParametersPrimitiveAllRepo repo,
+                        [FromServices] ISessionFactory<IAddEmployeeSession> sessionFactory,
+                        [FromBody] EmployeeDto employee) => await repo.CreateWithFluentValidationAsync(employee, sessionFactory));
+
+        return app;
+    }
 }
