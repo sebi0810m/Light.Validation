fe8fb18b3dd1eccad6f8536c2598c3c645aada0e
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-04 15:07:15 +0200

    Implemented CreateWithFluentValidationAsync method to repo

--- Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/ParametersPrimitiveAllRepo.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/ParametersPrimitiveAllRepo.cs
@@ -1,5 +1,8 @@
 ﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
+using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll.Validators;
 using Bachelor.Thesis.Benchmarking.WebApi.Repository;
+using Bachelor.Thesis.Benchmarking.WebApi.Validation;
+using Synnotech.AspNetCore.MinimalApis.Responses;
 using Synnotech.DatabaseAbstractions;
 
 namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
@@ -8,9 +11,15 @@ public class ParametersPrimitiveAllRepo : IRepository<EmployeeDto, IAddEmployeeS
 {
     public const string Url = "/api/primitive/all/";
 
-    public Task<IResult> CreateWithFluentValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    public async Task<IResult> CreateWithFluentValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
     {
-        throw new NotImplementedException();
+        var errors = new FluentValidator<EmployeeDto>(new FluentValidator(), value).PerformValidation();
+        if (!errors.IsValid)
+            return Response.ValidationProblem(ParseValidationResultsToCorrectType.ParseFluentValidationResults(errors));
+
+        value = await InsertEmployeeIntoDatabase(value, sessionFactory);
+
+        return Response.Created($"{Url}{value.Id}", value);
     }
 
     public Task<IResult> CreateWithLightValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
@@ -26,5 +35,13 @@ public class ParametersPrimitiveAllRepo : IRepository<EmployeeDto, IAddEmployeeS
     public Task<IResult> GetObjectByIdAsync(int id, ISessionFactory<IGetEmployeeSession> sessionFactory)
     {
         throw new NotImplementedException();
+    }
+
+    private async Task<EmployeeDto> InsertEmployeeIntoDatabase(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    {
+        await using var session = await sessionFactory.OpenSessionAsync();
+        value.Id = await session.InsertEmployeeAsync(value);
+        await session.SaveChangesAsync();
+        return value;
     }
 }
