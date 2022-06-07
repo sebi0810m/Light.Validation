f4c0fc0fe750c63f70bfe3a21ab60074e37df67b
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-07 17:14:10 +0200

    Added newCustomerDto for data casting before database storing

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/NewCustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
+
+public class NewCustomerDto
+{
+    public Guid CustomerId { get; set; }
+
+    public string User { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+}
