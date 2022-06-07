049243f5d55edbbe1d2ab37209429c94df993ea3
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-07 17:13:34 +0200

    Added JsonConverter for easier storing of data

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Database/JsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Database;
+
+public static class JsonConverter
+{
+    public static JsonSerializerOptions Options { get; } = new ()
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    public static string Serialize(object value)
+    {
+        return JsonSerializer.Serialize(value, Options);
+    }
+
+    public static object? Deserialize(string value, Type T)
+    {
+        var deserializedObject = JsonSerializer.Deserialize(value, T);
+
+        // TODO: cast object into right type
+
+        return deserializedObject;
+    }
+}
