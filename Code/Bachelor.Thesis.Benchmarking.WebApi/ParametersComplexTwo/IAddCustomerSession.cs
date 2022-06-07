f9e3ca922bdd453c5dda92e71fe552da8c979c52
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-07 17:14:22 +0200

    Added IAddCustomerSession interface and class

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/IAddCustomerSession.cs
@@ -0,0 +1,9 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersComplexTwo.Dto;
+using Synnotech.DatabaseAbstractions;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
+
+public interface IAddCustomerSession : IAsyncSession
+{
+    public Task<object> InsertEmployeeAsync(CustomerDto customer);
+}
---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/LinqToDbAddCustomerSession.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Bachelor.Thesis.Benchmarking.ParametersComplexTwo.Dto;
+using Bachelor.Thesis.Benchmarking.WebApi.Database;
+using LinqToDB;
+using LinqToDB.Data;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
+
+public class LinqToDbAddCustomerSession : AsyncSession, IAddCustomerSession
+{
+    public LinqToDbAddCustomerSession(DataConnection dataConnection, IsolationLevel transactionLevel = IsolationLevel.Serializable) : base(dataConnection, transactionLevel) { }
+
+    public Task<object> InsertEmployeeAsync(CustomerDto customer)
+    {
+        var newCustomer = new NewCustomerDto()
+        {
+            CustomerId = customer.CustomerId
+        };
+
+        newCustomer.User = JsonConverter.Serialize(customer.User);
+        newCustomer.Address = JsonConverter.Serialize(customer.Address);
+
+        return DataConnection.InsertWithIdentityAsync(customer);
+    }
+}
