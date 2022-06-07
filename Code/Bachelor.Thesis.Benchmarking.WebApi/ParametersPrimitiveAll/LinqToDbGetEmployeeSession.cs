08ec33306a5d141c34abd1ff4a7960fd7843c607
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-07 16:44:00 +0200

    Implemented getEmployeeSession

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/LinqToDbGetEmployeeSession.cs
@@ -0,0 +1,15 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersPrimitiveAll;
+using LinqToDB;
+using LinqToDB.Data;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
+
+public class LinqToDbGetEmployeeSession : AsyncReadOnlySession, IGetEmployeeSession
+{
+    public LinqToDbGetEmployeeSession(DataConnection dataConnection) : base(dataConnection) { }
+
+    public Task<EmployeeDto?> GetEmployeeByIdAsync(Guid employeeId) =>
+        DataConnection.GetTable<EmployeeDto>()
+                      .FirstOrDefaultAsync(employee => employee.Id == employeeId);
+}
