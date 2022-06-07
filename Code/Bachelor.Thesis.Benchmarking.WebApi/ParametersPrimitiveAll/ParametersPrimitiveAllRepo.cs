d977f9eccfd8039b31eb950ed5225af60bed2912
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-07 16:39:31 +0200

    Implemented missing Creation and Select methods for Api calls

--- Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/ParametersPrimitiveAllRepo.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/ParametersPrimitiveAllRepo.cs
@@ -7,11 +7,27 @@ using Synnotech.DatabaseAbstractions;
 
 namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
 
-public class ParametersPrimitiveAllRepo : IRepository<EmployeeDto, IAddEmployeeSession, IGetEmployeeSession>
+public class ParametersPrimitiveAllRepo : IRepository<EmployeeDto, Guid, IAddEmployeeSession, IGetEmployeeSession>
 {
     public const string Url = "/api/primitive/all/";
 
-    public async Task<IResult> CreateWithFluentValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    public async Task<IResult> CreateWithLightValidationAsync(
+        EmployeeDto value,
+        ISessionFactory<IAddEmployeeSession> sessionFactory)
+    {
+        var errors = new LightValidator<EmployeeDto>(new LightValidator(), value).PerformValidation();
+
+        if (!errors.IsValid)
+            return Response.ValidationProblem(ParseValidationResultsToCorrectType.ParseLightValidationResults(errors));
+
+        value = await InsertEmployeeIntoDatabase(value, sessionFactory);
+
+        return Response.Created($"{Url}{value.Id}", value);
+    }
+
+    public async Task<IResult> CreateWithFluentValidationAsync(
+        EmployeeDto value,
+        ISessionFactory<IAddEmployeeSession> sessionFactory)
     {
         var errors = new FluentValidator<EmployeeDto>(new FluentValidator(), value).PerformValidation();
         if (!errors.IsValid)
@@ -22,26 +38,37 @@ public class ParametersPrimitiveAllRepo : IRepository<EmployeeDto, IAddEmployeeS
         return Response.Created($"{Url}{value.Id}", value);
     }
 
-    public Task<IResult> CreateWithLightValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    public async Task<IResult> CreateWithModelValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
     {
-        throw new NotImplementedException();
+        var errors = new ModelValidator<EmployeeDto>(value).PerformValidation();
+
+        if (errors.Count != 0)
+            return Response.ValidationProblem(ParseValidationResultsToCorrectType.ParseModelValidationResults(errors));
+
+        value = await InsertEmployeeIntoDatabase(value, sessionFactory);
+
+        return Response.Created($"{Url}{value.Id}", value);
     }
 
-    public Task<IResult> CreateWithModelValidationAsync(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
+    public async Task<IResult> GetObjectByIdAsync(Guid id, ISessionFactory<IGetEmployeeSession> sessionFactory)
     {
-        throw new NotImplementedException();
-    }
+        await using var session = await sessionFactory.OpenSessionAsync();
 
-    public Task<IResult> GetObjectByIdAsync(int id, ISessionFactory<IGetEmployeeSession> sessionFactory)
-    {
-        throw new NotImplementedException();
+        var value = await session.GetEmployeeByIdAsync(id);
+
+        if(value == null) 
+            return Response.NotFound();
+
+        return Response.Ok(value);
     }
 
     private async Task<EmployeeDto> InsertEmployeeIntoDatabase(EmployeeDto value, ISessionFactory<IAddEmployeeSession> sessionFactory)
     {
         await using var session = await sessionFactory.OpenSessionAsync();
+
         value.Id = (Guid) await session.InsertEmployeeAsync(value);
         await session.SaveChangesAsync();
+
         return value;
     }
 }
