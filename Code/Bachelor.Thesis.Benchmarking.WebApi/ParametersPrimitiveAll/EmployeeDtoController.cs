6fefbfada07836a6e8af2648dc3dd0096a300ea4
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-07 16:44:24 +0200

    Added api endpoints for all validators and the get request

--- Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/EmployeeDtoController.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersPrimitiveAll/EmployeeDtoController.cs
@@ -9,16 +9,30 @@ public static class EmployeeDtoController
 {
     public static IServiceCollection AddEmployeeDtoServices(this IServiceCollection services) =>
         services.AddSingleton<ParametersPrimitiveAllRepo>()
-                .AddSessionFactoryFor<IAddEmployeeSession, LinqToDbAddEmployeeSession>();
+                .AddSessionFactoryFor<IAddEmployeeSession, LinqToDbAddEmployeeSession>()
+                .AddSessionFactoryFor<IGetEmployeeSession, LinqToDbGetEmployeeSession>();
 
     public static WebApplication AddEmployeeDtoEndpoints(this WebApplication app)
     {
         var defaultUrl = ParametersPrimitiveAllRepo.Url;
 
+        app.MapPost($"{defaultUrl}light", async (
+                        [FromServices] ParametersPrimitiveAllRepo repo,
+                        [FromServices] ISessionFactory<IAddEmployeeSession> sessionFactory,
+                        [FromBody] EmployeeDto employee) => await repo.CreateWithLightValidationAsync(employee, sessionFactory));
         app.MapPost($"{defaultUrl}fluent", async (
                         [FromServices] ParametersPrimitiveAllRepo repo,
                         [FromServices] ISessionFactory<IAddEmployeeSession> sessionFactory,
                         [FromBody] EmployeeDto employee) => await repo.CreateWithFluentValidationAsync(employee, sessionFactory));
+        app.MapPost($"{defaultUrl}model", async (
+                        [FromServices] ParametersPrimitiveAllRepo repo,
+                        [FromServices] ISessionFactory<IAddEmployeeSession> sessionFactory,
+                        [FromBody] EmployeeDto employee) => await repo.CreateWithModelValidationAsync(employee, sessionFactory));
+
+        app.MapGet(defaultUrl + "{id}", async (
+                       [FromServices] ParametersPrimitiveAllRepo repo,
+                       [FromServices] ISessionFactory<IGetEmployeeSession> sessionFactory,
+                       Guid id) => await repo.GetObjectByIdAsync(id, sessionFactory));
 
         return app;
     }
