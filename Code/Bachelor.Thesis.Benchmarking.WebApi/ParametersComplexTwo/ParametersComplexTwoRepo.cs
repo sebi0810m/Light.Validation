26d9229b8e2e3310250985791a5d7956bf0c9a13
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-08 16:46:38 +0200

    Added Repo for ComplexTwo object

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/ParametersComplexTwoRepo.cs
@@ -0,0 +1,30 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersComplexTwo.Dto;
+using Bachelor.Thesis.Benchmarking.WebApi.Repository;
+using Synnotech.DatabaseAbstractions;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
+
+public class ParametersComplexTwoRepo : IRepository<CustomerDto, Guid, IAddCustomerSession, IGetCustomerSession>
+{
+    public const string Url = "/api/complex/two/";
+
+    public Task<IResult> CreateWithLightValidationAsync(CustomerDto value, ISessionFactory<IAddCustomerSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> CreateWithFluentValidationAsync(CustomerDto value, ISessionFactory<IAddCustomerSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> CreateWithModelValidationAsync(CustomerDto value, ISessionFactory<IAddCustomerSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> GetObjectByIdAsync(Guid id, ISessionFactory<IGetCustomerSession> sessionFactory)
+    {
+        throw new NotImplementedException();
+    }
+}
