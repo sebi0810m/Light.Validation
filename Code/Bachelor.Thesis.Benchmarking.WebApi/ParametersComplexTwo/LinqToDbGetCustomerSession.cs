4c8f53088c33a1ae7171d4e6d5b6c79bf61749ea
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-08 17:10:46 +0200

    Added LinqToDbGetCustomerSession

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/LinqToDbGetCustomerSession.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Bachelor.Thesis.Benchmarking.ParametersComplexTwo.Dto;
+using LinqToDB.Data;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
+
+public class LinqToDbGetCustomerSession : AsyncReadOnlySession, IGetCustomerSession
+{
+    public LinqToDbGetCustomerSession(DataConnection dataConnection) : base(dataConnection) { }
+    public Task<CustomerDto?> GetEmployeeByIdAsync(Guid employeeId)
+    {
+        throw new NotImplementedException();
+    }
+}
