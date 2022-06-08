a464800662cf376f244170f2e17f66cc0c738650
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-08 17:11:42 +0200

    Included ParametersComplexTwo in Di Container

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Startup/DiContainerSetup.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Startup/DiContainerSetup.cs
@@ -1,4 +1,5 @@
-﻿using Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
+﻿using Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
+using Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveAll;
 using Bachelor.Thesis.Benchmarking.WebApi.ParametersPrimitiveTwo;
 
 namespace Bachelor.Thesis.Benchmarking.WebApi.Startup;
@@ -8,6 +9,7 @@ public static class DiContainerSetup
     public static IServiceCollection AddServicesForDtoInsertionIntoDatabase(this IServiceCollection services)
     {
         return services.AddUserDtoServices()
-                       .AddEmployeeDtoServices();
+                       .AddEmployeeDtoServices()
+                       .AddCustomerDtoServices();
     }
 }
