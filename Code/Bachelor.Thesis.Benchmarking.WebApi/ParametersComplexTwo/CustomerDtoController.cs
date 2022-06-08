d0f2e6dba09a25c120255102b8a9cebf1dd0c9d7
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-08 17:11:19 +0200

    Added CustomerDtoController

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/CustomerDtoController.cs
@@ -0,0 +1,39 @@
+﻿using Bachelor.Thesis.Benchmarking.ParametersComplexTwo.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Synnotech.DatabaseAbstractions;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
+
+public static class CustomerDtoController
+{
+    public static IServiceCollection AddCustomerDtoServices(this IServiceCollection services) =>
+        services.AddSingleton<ParametersComplexTwoRepo>()
+                .AddSessionFactoryFor<IAddCustomerSession, LinqToDbAddCustomerSession>()
+                .AddSessionFactoryFor<IGetCustomerSession, LinqToDbGetCustomerSession>();
+
+    public static WebApplication AddCustomerDtoEndpoints(this WebApplication app)
+    {
+        var defaultUrl = ParametersComplexTwoRepo.Url;
+
+        app.MapPost($"{defaultUrl}light", async (
+                        [FromServices] ParametersComplexTwoRepo repo,
+                        [FromServices] ISessionFactory<IAddCustomerSession> sessionFactory,
+                        [FromBody] CustomerDto customer) => await repo.CreateWithLightValidationAsync(customer, sessionFactory));
+        app.MapPost($"{defaultUrl}fluent", async (
+                        [FromServices] ParametersComplexTwoRepo repo,
+                        [FromServices] ISessionFactory<IAddCustomerSession> sessionFactory,
+                        [FromBody] CustomerDto customer) => await repo.CreateWithFluentValidationAsync(customer, sessionFactory));
+        app.MapPost($"{defaultUrl}model", async (
+                        [FromServices] ParametersComplexTwoRepo repo,
+                        [FromServices] ISessionFactory<IAddCustomerSession> sessionFactory,
+                        [FromBody] CustomerDto customer) => await repo.CreateWithModelValidationAsync(customer, sessionFactory));
+
+        app.MapGet(defaultUrl + "{id}", async (
+                       [FromServices] ParametersComplexTwoRepo repo,
+                       [FromServices] ISessionFactory<IGetCustomerSession> sessionFactory,
+                       Guid id) => await repo.GetObjectByIdAsync(id, sessionFactory));
+
+        return app;
+    }
+}
