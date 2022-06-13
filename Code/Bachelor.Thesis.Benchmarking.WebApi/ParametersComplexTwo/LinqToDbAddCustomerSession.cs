39f6e7e8aefdd8fa520693e854d19a66a2d5ec3c
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-13 12:30:29 +0200

    Changed name of method, removed test code

--- Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/LinqToDbAddCustomerSession.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/LinqToDbAddCustomerSession.cs
@@ -10,7 +10,7 @@ public class LinqToDbAddCustomerSession : AsyncSession, IAddCustomerSession
 {
     public LinqToDbAddCustomerSession(DataConnection dataConnection) : base(dataConnection) { }
 
-    public Task<object> InsertEmployeeAsync(CustomerDto customer)
+    public Task<object> InsertCustomerAsync(CustomerDto customer)
     {
         var serializedCustomer = new NewCustomerDto
         {
@@ -18,20 +18,7 @@ public class LinqToDbAddCustomerSession : AsyncSession, IAddCustomerSession
             User = JsonConvert.SerializeObject(customer.User),
             Address = JsonConvert.SerializeObject(customer.Address)
         };
-
-        // TestCode for deserializer -> does it work?
-        var deserializedCustomer = new CustomerDto
-        {
-            CustomerId = serializedCustomer.CustomerId,
-            User = JsonConvert.DeserializeObject<User>(serializedCustomer.User),
-            Address = JsonConvert.DeserializeObject<Address>(serializedCustomer.Address)
-        };
-
-        Console.WriteLine("\n\nOriginal Customer object: \n" + customer);
-        Console.WriteLine("\n\nNewCustomer object: \n" + serializedCustomer);
-        Console.WriteLine("\n\nDeserialized customer object: \n" + deserializedCustomer);
-        // end of TestCode
-
+        
         return DataConnection.InsertWithIdentityAsync(serializedCustomer);
     }
 }
