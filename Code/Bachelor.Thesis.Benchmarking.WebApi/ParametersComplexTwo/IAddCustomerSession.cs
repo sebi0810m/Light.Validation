fb481eb2af88fde5ec71f5a8d7a21b59fbaf78a5
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-13 12:29:48 +0200

    Renamed method in IAddCustomerSession

--- Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/IAddCustomerSession.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/ParametersComplexTwo/IAddCustomerSession.cs
@@ -5,5 +5,5 @@ namespace Bachelor.Thesis.Benchmarking.WebApi.ParametersComplexTwo;
 
 public interface IAddCustomerSession : IAsyncSession
 {
-    public Task<object> InsertEmployeeAsync(CustomerDto customer);
+    public Task<object> InsertCustomerAsync(CustomerDto customer);
 }
