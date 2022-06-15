781f5f948a64055172e55bfcd4ec9824afc14081
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-15 16:18:46 +0200

    Implemented get collection complex

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Cases/CollectionComplex/LinqToDbGetCollectionComplex.cs
@@ -0,0 +1,14 @@
+﻿using LinqToDB;
+using LinqToDB.Data;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Cases.CollectionComplex;
+
+public class LinqToDbGetCollectionComplex : AsyncReadOnlySession, IGetCollectionComplexSession
+{
+    public LinqToDbGetCollectionComplex(DataConnection dataConnection) : base(dataConnection) { }
+
+    public Task<SerializedCollectionComplex?> GetCollectionComplexAsync(Guid id) =>
+        DataConnection.GetTable<SerializedCollectionComplex>()
+                      .FirstOrDefaultAsync(collection => collection.Id == id);
+}
