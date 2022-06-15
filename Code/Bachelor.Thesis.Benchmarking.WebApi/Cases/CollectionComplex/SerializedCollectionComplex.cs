593fa64b552dc934081e13abea11a933481e7204
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-15 16:07:44 +0200

    Added serializable class

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Cases/CollectionComplex/SerializedCollectionComplex.cs
@@ -0,0 +1,10 @@
+﻿namespace Bachelor.Thesis.Benchmarking.WebApi.Cases.CollectionComplex;
+
+public class SerializedCollectionComplex
+{
+    public Guid Id { get; set; }
+
+    public string OrderDetailsList { get; set; } = string.Empty;
+
+    public string ArticleList { get; set; } = string.Empty;
+}
