38e110c483fdaa0cb7239a062cbb506d3b0d2f0a
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-15 16:15:18 +0200

    Implemented addCollectionComplexSession

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Cases/CollectionComplex/LinqToDbAddCollectionComplexSession.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using Bachelor.Thesis.Benchmarking.CollectionComplex.Dto;
+using LinqToDB;
+using LinqToDB.Data;
+using Newtonsoft.Json;
+using Synnotech.Linq2Db;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Cases.CollectionComplex;
+
+public class LinqToDbAddCollectionComplexSession : AsyncSession, IAddCollectionComplexSession
+{
+    public LinqToDbAddCollectionComplexSession(DataConnection dataConnection) : base(dataConnection) { }
+
+    public Task<object> InsertCollectionComplexAsync(CollectionComplexDto collection)
+    {
+        var serializedCollectionComplex = new SerializedCollectionComplex
+        {
+            Id = collection.Id,
+            OrderDetailsList = JsonConvert.SerializeObject(collection.OrderDetailsList),
+            ArticleList = JsonConvert.SerializeObject(collection.ArticleList)
+        };
+
+        return DataConnection.InsertWithIdentityAsync(serializedCollectionComplex);
+    }
+}
