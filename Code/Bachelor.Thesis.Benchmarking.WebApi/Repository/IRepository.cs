5500d18b4f6e14a49c3c748f8815dae3b55ab7ee
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-28 15:10:32 +0200

    Changed IRepository to include validators in method signature

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Repository/IRepository.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Repository/IRepository.cs
@@ -2,11 +2,11 @@
 
 namespace Bachelor.Thesis.Benchmarking.WebApi.Repository;
 
-public interface IRepository<in T, in TId, TInsertObjectSession, TGetObjectSession>
+public interface IRepository<in T, in TId, in TLightValidator, in TFluentValidator, TInsertObjectSession, TGetObjectSession>
 {
-    Task<IResult> CreateWithLightValidationAsync(T value, ISessionFactory<TInsertObjectSession> sessionFactory);
+    Task<IResult> CreateWithLightValidationAsync(T value, TLightValidator validator, ISessionFactory<TInsertObjectSession> sessionFactory);
 
-    Task<IResult> CreateWithFluentValidationAsync(T value, ISessionFactory<TInsertObjectSession> sessionFactory);
+    Task<IResult> CreateWithFluentValidationAsync(T value, TFluentValidator validator, ISessionFactory<TInsertObjectSession> sessionFactory);
 
     Task<IResult> CreateWithModelValidationAsync(T value, ISessionFactory<TInsertObjectSession> sessionFactory);
 
