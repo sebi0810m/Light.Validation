0242f058ffcc673497dcac2ae77b4b1655d5282e
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-28 16:49:58 +0200

    Added FluentValidatorHelper to unify ToModelStateDictionary method

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Validation/FluentValidatorHelper.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Bachelor.Thesis.Benchmarking.WebApi.Validation;
+
+public static class FluentValidatorHelper
+{
+    public static ModelStateDictionary ToModelStateDictionary(this ValidationResult result)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+        result.AddToModelState(modelStateDictionary, string.Empty);
+        return modelStateDictionary;
+    }
+}
