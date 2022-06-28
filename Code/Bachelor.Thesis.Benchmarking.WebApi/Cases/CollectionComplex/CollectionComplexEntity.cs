78632d5320499efe5f4dd64291cfb7f9d3835605
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-06-28 16:10:09 +0200

    Added int id to entity

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Cases/CollectionComplex/CollectionComplexEntity.cs
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Cases/CollectionComplex/CollectionComplexEntity.cs
@@ -2,7 +2,9 @@
 
 public class CollectionComplexEntity
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
+
+    public Guid Guid { get; set; }
 
     public string OrderDetailsList { get; set; } = string.Empty;
 
