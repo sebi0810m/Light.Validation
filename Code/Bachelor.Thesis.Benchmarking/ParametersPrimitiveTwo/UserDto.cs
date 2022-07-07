db8c3d9c7186f2e7d60ded9a10d77086491610a6
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-07 11:50:59 +0200

    Changed validators to display more error messages with given invalid input

--- Code/Bachelor.Thesis.Benchmarking/ParametersPrimitiveTwo/UserDto.cs
+++ Code/Bachelor.Thesis.Benchmarking/ParametersPrimitiveTwo/UserDto.cs
@@ -5,13 +5,13 @@ namespace Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
 public class UserDto
 {
     public static UserDto ValidDto = new () { Id = 42, Name = "John Doe" };
-    public static UserDto InvalidDto = new () { Id = -1, Name = "    J  " };
+    public static UserDto InvalidDto = new () { Id = -1, Name = "J" };
 
     [Required]
     [Range(0, int.MaxValue)]
     public int Id { get; set; }
 
     [Required]
-    [StringLength(80, MinimumLength = 1)]
-    public string Name { get; set; } = String.Empty;
+    [StringLength(80, MinimumLength = 2)]
+    public string Name { get; set; } = string.Empty;
 }
--- Code/Bachelor.Thesis.Benchmarking/ParametersPrimitiveTwo/Validators/FluentValidator.cs
+++ Code/Bachelor.Thesis.Benchmarking/ParametersPrimitiveTwo/Validators/FluentValidator.cs
@@ -8,7 +8,7 @@ public class FluentValidator : AbstractValidator<UserDto>
     public FluentValidator()
     {
         RuleFor(dto => dto.Id).GreaterThan(0).LessThan(int.MaxValue);
-        RuleFor(dto => dto.Name).NotEmpty().MaximumLength(80);
+        RuleFor(dto => dto.Name).NotEmpty().Length(min: 2, max: 80);
     }
 
     protected override bool PreValidate(ValidationContext<UserDto> context, ValidationResult result)
