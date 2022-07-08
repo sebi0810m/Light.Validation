c6aa3905d6dcef04284bcce50f36b29158c118b2
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-08 17:06:46 +0200

    Renamed new test class to ParametersPrimitiveTwoParsingTest

--- Code/Bachelor.Thesis.Tests/Validators/ParametersPrimitiveTwoParsingTest.cs
+++ Code/Bachelor.Thesis.Tests/Validators/ParametersPrimitiveTwoParsingTest.cs
@@ -1,23 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo;
 using Bachelor.Thesis.Benchmarking.ParametersPrimitiveTwo.Validators;
-using FluentValidation.AspNetCore;
 using Light.GuardClauses;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Bachelor.Thesis.Tests.Validators;
 
-public class ParametersPrimitiveTwoModelStateTest
+public class ParametersPrimitiveTwoParsingTest
 {
     private readonly UserDto _invalidUser = UserDto.InvalidDto;
 
     public ITestOutputHelper Output;
 
-    public ParametersPrimitiveTwoModelStateTest(ITestOutputHelper output)
+    public ParametersPrimitiveTwoParsingTest(ITestOutputHelper output)
     {
         Output = output;
     }
@@ -49,7 +46,7 @@ public class ParametersPrimitiveTwoModelStateTest
     {
         var errors = new List<ValidationResult>();
         var result = Validator.TryValidateObject(_invalidUser, new ValidationContext(_invalidUser), errors, true);
-        
+
         Output.WriteLine(Json.Serialize(errors.ModelWriteInLightValidationResult(_invalidUser))); // TODO: IsValid is true => should be false
 
         result.MustBe(false);
