9e82bd5687eb93d14cd3ee935f41a0afe5143471
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-18 18:07:13 +0200

    Increased virtual users in test

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveTwo/InvalidTest.js
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveTwo/InvalidTest.js
@@ -3,14 +3,14 @@ import { check, sleep } from "k6";
 
 export const options = {
     stages: [
-        { duration: "20s", target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 10 seconds.
-        { duration: "3m", target: 10 }, // stay at 10 users for 1 minutes
+        { duration: "20s", target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 20 seconds.
+        { duration: "3m", target: 100 }, // stay at 100 users for 3 minutes
         { duration: "20s", target: 0 } // ramp-down to 0 users
     ]
 };
 
 export default function() {
-    const url = `https://localhost:7089/api/primitive/two/${window.__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx InvalidTest.js
+    const url = `https://localhost:7089/api/primitive/two/${__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx InvalidTest.js
 
     const payload = JSON.stringify({
         id: "-8",
