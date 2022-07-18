6efe3ee9bf0f74139bc794118199e1428d81fd37
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-18 16:54:31 +0200

    Cleaned up code

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveTwo/InvalidTest.js
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveTwo/InvalidTest.js
@@ -1,31 +1,31 @@
-﻿import http from 'k6/http';
-import { check, sleep } from 'k6';
+﻿import http from "k6/http";
+import { check, sleep } from "k6";
 
 export const options = {
     stages: [
-        { duration: '20s', target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 10 seconds.
-        { duration: '3m', target: 10 }, // stay at 10 users for 1 minutes
-        { duration: '20s', target: 0 }, // ramp-down to 0 users
+        { duration: "20s", target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 10 seconds.
+        { duration: "3m", target: 10 }, // stay at 10 users for 1 minutes
+        { duration: "20s", target: 0 } // ramp-down to 0 users
     ]
 };
 
 export default function() {
-    const url = `https://localhost:7089/api/primitive/two/${__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx InvalidTest.js
+    const url = `https://localhost:7089/api/primitive/two/${window.__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx InvalidTest.js
 
     const payload = JSON.stringify({
-        id: '-8',
-        name: 'J'
+        id: "-8",
+        name: "J"
     });
 
     const params = {
         headers: {
-            'Content-Type': 'application/json',
-        },
+            'Content-Type': "application/json"
+        }
     };
 
     const response = http.post(url, payload, params);
 
-    check(response, {'Status was 400 Bad Request': (res) => res.status == 400});
+    check(response, {'Status was 400 Bad Request': (res) => res.status === 400});
 
     sleep(1);
 }
--- Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveTwo/ValidTest.js
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveTwo/ValidTest.js
@@ -1,16 +1,16 @@
-﻿import http from 'k6/http';
-import { check, sleep } from 'k6';
+﻿import http from "k6/http";
+import { check, sleep } from "k6";
 
 export const options = {
     stages: [
-        { duration: '20s', target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 10 seconds.
-        { duration: '3m', target: 10 }, // stay at 10 users for 1 minutes
-        { duration: '20s', target: 0 }, // ramp-down to 0 users
+        { duration: "20s", target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 10 seconds.
+        { duration: "3m", target: 10 }, // stay at 10 users for 1 minutes
+        { duration: "20s", target: 0 } // ramp-down to 0 users
     ]
 };
 
 export default function() {
-    const url = `https://localhost:7089/api/primitive/two/${__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx ValidTest.js
+    const url = `https://localhost:7089/api/primitive/two/${window.__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx ValidTest.js
 
     const payload = JSON.stringify({
         id: "42",
@@ -19,13 +19,13 @@ export default function() {
 
     const params = {
         headers: {
-            'Content-Type': 'application/json',
-        },
+            'Content-Type': "application/json"
+        }
     };
 
     const response = http.post(url, payload, params);
 
-    check(response, {'Status was 201 Created': (res) => res.status == 201});
+    check(response, {'Status was 201 Created': (res) => res.status === 201});
 
     sleep(1);
 }
