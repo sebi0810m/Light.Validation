a51b89e8584cf5db82654450903a757f04e875dd
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-18 16:38:03 +0200

    Added check to k6 test, added params to http post request, changed options for k6

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Tests/ParametersPrimitiveTwo/InvalidTest.js
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Tests/ParametersPrimitiveTwo/InvalidTest.js
@@ -1,20 +1,31 @@
 ﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
 
-export let options = {
-    insecureSkipTLSVerify: true,
-    noConnectionReuse: false,
-    vus: 1,
-    duration: '10s'
+export const options = {
+    stages: [
+        { duration: '20s', target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 10 seconds.
+        { duration: '3m', target: 10 }, // stay at 10 users for 1 minutes
+        { duration: '20s', target: 0 }, // ramp-down to 0 users
+    ]
 };
 
 export default function() {
-    // ReSharper disable once UseOfImplicitGlobalInFunctionScope
-    const url = `https://localhost:7089/api/primitive/two/${__ENV.VALIDATION_NAME}`;
+    const url = `https://localhost:7089/api/primitive/two/${__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx InvalidTest.js
 
     const payload = JSON.stringify({
         id: '-8',
         name: 'J'
     });
 
-    http.post(url, payload);
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = http.post(url, payload, params);
+
+    check(response, {'Status was 400 Bad Request': (res) => res.status == 400});
+
+    sleep(1);
 }
--- Code/Bachelor.Thesis.Benchmarking.WebApi/Tests/ParametersPrimitiveTwo/ValidTest.js
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Tests/ParametersPrimitiveTwo/ValidTest.js
@@ -1,28 +1,31 @@
 ﻿import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '20s', target: 4 }, // simulate ramp-up of traffic from 1 to 4 users over 10 seconds.
-        { duration: '3m', target: 4 }, // stay at 4 users for 1 minutes
+        { duration: '20s', target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 10 seconds.
+        { duration: '3m', target: 10 }, // stay at 10 users for 1 minutes
         { duration: '20s', target: 0 }, // ramp-down to 0 users
-    ],
-    thresholds: {
-        'http_req_duration': ['p(99)<1500'], // 99% of requests must complete below 1.5s
-        'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
-    },
+    ]
 };
 
 export default function() {
-    // ReSharper disable once UseOfImplicitGlobalInFunctionScope
     const url = `https://localhost:7089/api/primitive/two/${__ENV.VALIDATION_NAME}`; // use with 'k6 run -e VALIDATION_NAME=xxxx ValidTest.js
 
     const payload = JSON.stringify({
-        id: '42',
-        name: 'John Doe'
+        id: "42",
+        name: "John Doe"
     });
 
-    http.post(url, payload);
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = http.post(url, payload, params);
+
+    check(response, {'Status was 201 Created': (res) => res.status == 201});
 
     sleep(1);
 }
