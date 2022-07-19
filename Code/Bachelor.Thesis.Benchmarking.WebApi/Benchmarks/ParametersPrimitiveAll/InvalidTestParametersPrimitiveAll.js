6ff244aec8f2d9a39d086499cbf6de8ab2e55fc6
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-19 15:50:30 +0200

    Added k6 test scripts for parametersPrimitiveAll

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveAll/InvalidTestParametersPrimitiveAll.js
@@ -0,0 +1,27 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { baseOptions, baseParametersPrimitiveAllUrl, baseParams } from "../k6Base.js";
+
+export const options = baseOptions;
+
+export default function () {
+    const url = baseParametersPrimitiveAllUrl;
+
+    const payload = JSON.stringify({
+        name: "J",
+        position: "a",
+        department: 98,
+        weeklyWorkingHours: 50,
+        employeeId: 1023,
+        productivityScore: 120.1,
+        overtimeWorked: -110.23,
+        hourlySalary: 8.5,
+        dateEmployed: "2023-01-01"
+    });
+
+    const response = http.post(url, payload, baseParams);
+
+    check(response, { 'Status was 400 Bad Request': (res) => res.status === 400 });
+
+    sleep(1);
+}
---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersPrimitiveAll/ValidTestParametersPrimitiveAll.js
@@ -0,0 +1,27 @@
+﻿import http from "k6/http";
+import { check, sleep } from "k6";
+import { baseOptions, baseParametersPrimitiveAllUrl, baseParams } from "../k6Base.js";
+
+export const options = baseOptions;
+
+export default function () {
+    const url = baseParametersPrimitiveAllUrl;
+
+    const payload = JSON.stringify({
+        name: "John Doe",
+        position: "D",
+        department: 123,
+        weeklyWorkingHours: 40,
+        employeeId: 13370,
+        productivityScore: 42.4,
+        overtimeWorked: 69.9,
+        hourlySalary: 18.8,
+        dateEmployed: "2020-04-12"
+    });
+
+    const response = http.post(url, payload, baseParams);
+
+    check(response, { 'Status was 201 Created': (res) => res.status === 201 });
+
+    sleep(1);
+}
