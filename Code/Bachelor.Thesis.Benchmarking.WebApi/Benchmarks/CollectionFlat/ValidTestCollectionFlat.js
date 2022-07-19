917ea478fe5e9034193d5e3bd5b0056f63a60a66
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-19 15:50:46 +0200

    Added k6 test scripts for collectionFlat

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/CollectionFlat/ValidTestCollectionFlat.js
@@ -0,0 +1,28 @@
+﻿import http from "k6/http";
+import { check, sleep } from "k6";
+import { baseOptions, baseParametersCollectionFlatUrl, baseParams } from "../k6Base.js";
+
+export const options = baseOptions;
+
+export default function () {
+    const url = baseParametersCollectionFlatUrl;
+
+    const payload = JSON.stringify({
+        names: [
+            "John Doe",
+            "Max Muster",
+            "Foo Foo"
+        ],
+        availability: {
+            1234: true,
+            4568: false,
+            1250: true
+        }
+    });
+
+    const response = http.post(url, payload, baseParams);
+
+    check(response, { 'Status was 201 Created': (res) => res.status === 201 });
+
+    sleep(1);
+}
