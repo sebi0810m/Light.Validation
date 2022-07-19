303b0082b175ca91fd754a55f6146c277a30bcda
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-19 15:50:38 +0200

    Added k6 test scripts for parametesrComplexTwo

---
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/ParametersComplexTwo/InvalidTestParametersComplexTwo.js
@@ -0,0 +1,31 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { baseOptions, baseParametersComplexTwoUrl, baseParams } from "../k6Base.js";
+
+export const options = baseOptions;
+
+export default function () {
+    const url = baseParametersComplexTwoUrl;
+
+    const payload = JSON.stringify({
+        user: {
+            userName: "John",
+            password: "P4S$W0r",
+            name: "J",
+            email: "D",
+            age: 16
+        },
+        address: {
+            country: "",
+            region: "",
+            postalCode: 1000,
+            street: ""
+        }
+    });
+
+    const response = http.post(url, payload, baseParams);
+
+    check(response, { 'Status was 400 Bad Request': (res) => res.status === 400 });
+
+    sleep(1);
+}
