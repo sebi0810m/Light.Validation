be1dc006cbf726f774d3ad84d58088ea2e960c28
Sebastian Mohr <sebastianmohr96@gmail.com>
2022-07-21 09:50:34 +0200

    Fixed a few bugs in k6Test shell script, added remove result before moving the new one into result folder

--- Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/k6Tests.sh
+++ Code/Bachelor.Thesis.Benchmarking.WebApi/Benchmarks/k6Tests.sh
@@ -1,91 +1,121 @@
-k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveTwo\ValidParametersPrimitiveTwo.js
-mv result.json.\Results\ResultParametersPrimitiveTwoValidLight.json
+k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveTwo\ValidTestParametersPrimitiveTwo.js
+rm .\Results\ResultParametersPrimitiveTwoValidLight.json
+mv result.json .\Results\ResultParametersPrimitiveTwoValidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveTwo\ValidParametersPrimitiveTwo.js
-mv result.json.\Results\ResultParametersPrimitiveTwoValidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveTwo\ValidTestParametersPrimitiveTwo.js
+rm .\Results\ResultParametersPrimitiveTwoValidFluent.json
+mv result.json .\Results\ResultParametersPrimitiveTwoValidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveTwo\ValidParametersPrimitiveTwo.js
-mv result.json.\Results\ResultParametersPrimitiveTwoValidModel.json
+k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveTwo\ValidTestParametersPrimitiveTwo.js
+rm .\Results\ResultParametersPrimitiveTwoValidModel.json
+mv result.json .\Results\ResultParametersPrimitiveTwoValidModel.json
 
-k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveTwo\InvalidParametersPrimitiveTwo.js
-mv result.json.\Results\ResultParametersPrimitiveTwoInvalidLight.json
+k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveTwo\InvalidTestParametersPrimitiveTwo.js
+rm .\Results\ResultParametersPrimitiveTwoInvalidLight.json
+mv result.json .\Results\ResultParametersPrimitiveTwoInvalidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveTwo\InvalidParametersPrimitiveTwo.js
-mv result.json.\Results\ResultParametersPrimitiveTwoInvalidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveTwo\InvalidTestParametersPrimitiveTwo.js
+rm .\Results\ResultParametersPrimitiveTwoInvalidFluent.json
+mv result.json .\Results\ResultParametersPrimitiveTwoInvalidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveTwo\InvalidParametersPrimitiveTwo.js
-mv result.json.\Results\ResultParametersPrimitiveTwoInvalidModel.json
+k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveTwo\InvalidTestParametersPrimitiveTwo.js
+rm .\Results\ResultParametersPrimitiveTwoInvalidModel.json
+mv result.json .\Results\ResultParametersPrimitiveTwoInvalidModel.json
 
-k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveAll\ValidParametersPrimitiveAll.js
-mv result.json.\Results\ResultParametersPrimitiveAllValidLight.json
+k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveAll\ValidTestParametersPrimitiveAll.js
+rm .\Results\ResultParametersPrimitiveAllValidLight.json
+mv result.json .\Results\ResultParametersPrimitiveAllValidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveAll\ValidParametersPrimitiveAll.js
-mv result.json.\Results\ResultParametersPrimitiveAllValidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveAll\ValidTestParametersPrimitiveAll.js
+rm .\Results\ResultParametersPrimitiveAllValidFluent.json
+mv result.json .\Results\ResultParametersPrimitiveAllValidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveAll\ValidParametersPrimitiveAll.js
-mv result.json.\Results\ResultParametersPrimitiveAllValidModel.json
+k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveAll\ValidTestParametersPrimitiveAll.js
+rm .\Results\ResultParametersPrimitiveAllValidModel.json
+mv result.json .\Results\ResultParametersPrimitiveAllValidModel.json
 
-k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveAll\InvalidParametersPrimitiveAll.js
-mv result.json.\Results\ResultParametersPrimitiveAllInvalidLight.json
+k6 run -e VALIDATION_NAME=light .\ParametersPrimitiveAll\InvalidTestParametersPrimitiveAll.js
+rm .\Results\ResultParametersPrimitiveAllInvalidLight.json
+mv result.json .\Results\ResultParametersPrimitiveAllInvalidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveAll\InvalidParametersPrimitiveAll.js
-mv result.json.\Results\ResultParametersPrimitiveAllInvalidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\ParametersPrimitiveAll\InvalidTestParametersPrimitiveAll.js
+rm .\Results\ResultParametersPrimitiveAllInvalidFluent.json
+mv result.json .\Results\ResultParametersPrimitiveAllInvalidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveAll\InvalidParametersPrimitiveAll.js
-mv result.json.\Results\ResultParametersPrimitiveAllInvalidModel.json
+k6 run -e VALIDATION_NAME=model .\ParametersPrimitiveAll\InvalidTestParametersPrimitiveAll.js
+rm .\Results\ResultParametersPrimitiveAllInvalidModel.json
+mv result.json .\Results\ResultParametersPrimitiveAllInvalidModel.json
 
-k6 run -e VALIDATION_NAME=light .\ParametersComplexTwo\ValidParametersComplexTwo.js
-mv result.json.\Results\ResultParametersComplexTwoValidLight.json
+k6 run -e VALIDATION_NAME=light .\ParametersComplexTwo\ValidTestParametersComplexTwo.js
+rm .\Results\ResultParametersComplexTwoValidLight.json
+mv result.json .\Results\ResultParametersComplexTwoValidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\ParametersComplexTwo\ValidParametersComplexTwo.js
-mv result.json.\Results\ResultParametersComplexTwoValidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\ParametersComplexTwo\ValidTestParametersComplexTwo.js
+rm .\Results\ResultParametersComplexTwoValidFluent.json
+mv result.json .\Results\ResultParametersComplexTwoValidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\ParametersComplexTwo\ValidParametersComplexTwo.js
-mv result.json.\Results\ResultParametersComplexTwoValidModel.json
+k6 run -e VALIDATION_NAME=model .\ParametersComplexTwo\ValidTestParametersComplexTwo.js
+rm .\Results\ResultParametersComplexTwoValidModel.json
+mv result.json .\Results\ResultParametersComplexTwoValidModel.json
 
-k6 run -e VALIDATION_NAME=light .\ParametersComplexTwo\InvalidParametersComplexTwo.js
-mv result.json.\Results\ResultParametersComplexTwoInvalidLight.json
+k6 run -e VALIDATION_NAME=light .\ParametersComplexTwo\InvalidTestParametersComplexTwo.js
+rm .\Results\ResultParametersComplexTwoInvalidLight.json
+mv result.json .\Results\ResultParametersComplexTwoInvalidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\ParametersComplexTwo\InvalidParametersComplexTwo.js
-mv result.json.\Results\ResultParametersComplexTwoInvalidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\ParametersComplexTwo\InvalidTestParametersComplexTwo.js
+rm .\Results\ResultParametersComplexTwoInvalidFluent.json
+mv result.json .\Results\ResultParametersComplexTwoInvalidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\ParametersComplexTwo\InvalidParametersComplexTwo.js
-mv result.json.\Results\ResultParametersComplexTwoInvalidModel.json
+k6 run -e VALIDATION_NAME=model .\ParametersComplexTwo\InvalidTestParametersComplexTwo.js
+rm .\Results\ResultParametersComplexTwoInvalidModel.json
+mv result.json .\Results\ResultParametersComplexTwoInvalidModel.json
 
-k6 run -e VALIDATION_NAME=light .\CollectionFlat\ValidCollectionFlat.js
-mv result.json.\Results\ResultCollectionFlatValidLight.json
+k6 run -e VALIDATION_NAME=light .\CollectionFlat\ValidTestCollectionFlat.js
+rm .\Results\ResultCollectionFlatValidLight.json
+mv result.json .\Results\ResultCollectionFlatValidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\CollectionFlat\ValidCollectionFlat.js
-mv result.json.\Results\ResultCollectionFlatValidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\CollectionFlat\ValidTestCollectionFlat.js
+rm .\Results\ResultCollectionFlatValidFluent.json
+mv result.json .\Results\ResultCollectionFlatValidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\CollectionFlat\ValidCollectionFlat.js
-mv result.json.\Results\ResultCollectionFlatValidModel.json
+k6 run -e VALIDATION_NAME=model .\CollectionFlat\ValidTestCollectionFlat.js
+rm .\Results\ResultCollectionFlatValidModel.json
+mv result.json .\Results\ResultCollectionFlatValidModel.json
 
-k6 run -e VALIDATION_NAME=light .\CollectionFlat\InvalidCollectionFlat.js
-mv result.json.\Results\ResultCollectionFlatInvalidLight.json
+k6 run -e VALIDATION_NAME=light .\CollectionFlat\InvalidTestCollectionFlat.js
+rm .\Results\ResultCollectionFlatInvalidLight.json
+mv result.json .\Results\ResultCollectionFlatInvalidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\CollectionFlat\InvalidCollectionFlat.js
-mv result.json.\Results\ResultCollectionFlatInvalidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\CollectionFlat\InvalidTestCollectionFlat.js
+rm .\Results\ResultCollectionFlatInvalidFluent.json
+mv result.json .\Results\ResultCollectionFlatInvalidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\CollectionFlat\InvalidCollectionFlat.js
-mv result.json.\Results\ResultCollectionFlatInvalidModel.json
+k6 run -e VALIDATION_NAME=model .\CollectionFlat\InvalidTestCollectionFlat.js
+rm .\Results\ResultCollectionFlatInvalidModel.json
+mv result.json .\Results\ResultCollectionFlatInvalidModel.json
 
-k6 run -e VALIDATION_NAME=light .\CollectionComplex\ValidCollectionComplex.js
-mv result.json.\Results\ResultCollectionComplexValidLight.json
+k6 run -e VALIDATION_NAME=light .\CollectionComplex\ValidTestCollectionComplex.js
+rm .\Results\ResultCollectionComplexValidLight.json
+mv result.json .\Results\ResultCollectionComplexValidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\CollectionComplex\ValidCollectionComplex.js
-mv result.json.\Results\ResultCollectionComplexValidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\CollectionComplex\ValidTestCollectionComplex.js
+rm .\Results\ResultCollectionComplexValidFluent.json
+mv result.json .\Results\ResultCollectionComplexValidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\CollectionComplex\ValidCollectionComplex.js
-mv result.json.\Results\ResultCollectionComplexValidModel.json
+k6 run -e VALIDATION_NAME=model .\CollectionComplex\ValidTestCollectionComplex.js
+rm .\Results\ResultCollectionComplexValidModel.json
+mv result.json .\Results\ResultCollectionComplexValidModel.json
 
-k6 run -e VALIDATION_NAME=light .\CollectionComplex\InvalidCollectionComplex.js
-mv result.json.\Results\ResultCollectionComplexInvalidLight.json
+k6 run -e VALIDATION_NAME=light .\CollectionComplex\InvalidTestCollectionComplex.js
+rm .\Results\ResultCollectionComplexInvalidLight.json
+mv result.json .\Results\ResultCollectionComplexInvalidLight.json
 
-k6 run -e VALIDATION_NAME=fluent .\CollectionComplex\InvalidCollectionComplex.js
-mv result.json.\Results\ResultCollectionComplexInvalidFluent.json
+k6 run -e VALIDATION_NAME=fluent .\CollectionComplex\InvalidTestCollectionComplex.js
+rm .\Results\ResultCollectionComplexInvalidFluent.json
+mv result.json .\Results\ResultCollectionComplexInvalidFluent.json
 
-k6 run -e VALIDATION_NAME=model .\CollectionComplex\InvalidCollectionComplex.js
-mv result.json.\Results\ResultCollectionComplexInvalidModel.json
+k6 run -e VALIDATION_NAME=model .\CollectionComplex\InvalidTestCollectionComplex.js
+rm .\Results\ResultCollectionComplexInvalidModel.json
+mv result.json .\Results\ResultCollectionComplexInvalidModel.json
 
 echo done
